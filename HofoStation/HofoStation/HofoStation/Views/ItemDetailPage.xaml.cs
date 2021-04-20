262ae5fe5fdd2c2a07220b5a0e918e14b9e1dc14
TEY WEE KIT <wjz1012.1999@gmail.com>
2021-04-21 02:33:39 +0800

    Create Code File

---
+++ HofoStation/HofoStation/HofoStation/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using HofoStation.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace HofoStation.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
