20da539552040aee95a8ebe9169eb5e07b7c839a
TEY WEE KIT <wjz1012.1999@gmail.com>
2021-06-09 03:58:45 +0800

    Create Signal R Hub

---
+++ HofoChatHub/HofoChatHub/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HofoChatHub.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string chatID, string userID, string message)
+        {
+            await Clients.Group(chatID).SendAsync("ReceiveMessage", userID, message);
+        }
+    }
+}
